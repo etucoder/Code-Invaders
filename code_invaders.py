f73bc9e7fa434ed8eee6b8f299975f024ffedaa2
etucoder <ethu2020m@gmail.com>
2026-07-25 23:42:20 -0500

    Added Movement Code!

--- code_invaders.py
+++ code_invaders.py
@@ -8,7 +8,7 @@ clock = pygame.time.Clock()
 screen_color = (0,0,0)
 
 class Ship(pygame.sprite.Sprite):
-    def __init__(self,x,y,w,h,image_path,damage,hp):
+    def __init__(self,x,y,w,h,image_path,damage,hp,speed = 8):
         super().__init__()
         self.x = x
         self.y = y
@@ -19,7 +19,18 @@ class Ship(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = (x,y))
         self.damage = damage
         self.hp = hp
-
+        self.speed = speed
+    def move(self):
+        global keys
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+        elif keys[pygame.K_DOWN]:
+            self.rect.y += self.speed
+        elif keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+        elif keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+            
 pro_ships = pygame.sprite.Group()
 ship = Ship(100,100,40,40,"ship.png",1,10)
 pro_ships.add(ship)
@@ -34,4 +45,6 @@ while running:
     mouse_pos = pygame.mouse.get_pos()
     screen.fill(screen_color)
     pro_ships.draw(screen)
+    for ship in pro_ships:
+        ship.move()
     pygame.display.flip()
