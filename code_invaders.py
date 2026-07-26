fb19096fa437a1c0a38aa7dfc3558e298683b81f
etucoder <ethu2020m@gmail.com>
2026-07-25 23:07:17 -0500

    Added the ship sprite!

--- code_invaders.py
+++ code_invaders.py
@@ -3,3 +3,19 @@ pygame.init()
 WIDTH , HEIGHT = 1000 ,600
 FPS = 60
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
+running = True
+clock = pygame.time.Clock()
+screen_color = (0,0,0)
+
+
+
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        keys = pygame.key.get_pressed()
+        mouse_pos = pygame.mouse.get_pos()
+        screen.fill(screen_color)
+        pygame.display.flip()
