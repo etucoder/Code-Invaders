e73c4b9af74e9afdf9978e4d20d1c9dc4491ab9f
etucoder <ethu2020m@gmail.com>
2026-07-26 08:01:46 -0500

    Added the shooting code

--- code_invaders.py
+++ code_invaders.py
@@ -20,6 +20,8 @@ class Ship(pygame.sprite.Sprite):
         self.damage = damage
         self.hp = hp
         self.speed = speed
+        self.cooldown = 20
+        self.max_cooldown = self.cooldown
     def move(self):
         global keys
         if keys[pygame.K_UP]:
@@ -30,10 +32,49 @@ class Ship(pygame.sprite.Sprite):
             self.rect.x -= self.speed
         elif keys[pygame.K_RIGHT]:
             self.rect.x += self.speed
-            
+        if self.rect.x >= WIDTH - self.w:
+            self.rect.x = WIDTH - self.w
+        elif self.rect.x <= 0:
+            self.rect.x =  0
+
+        if self.rect.y <= 0.66 * HEIGHT:
+            self.rect.y = 0.66 * HEIGHT
+
+        if self.rect.y >= HEIGHT - self.h:
+            self.rect.y = HEIGHT - self.h
+
+    def shoot(self):
+        global lasers
+        if keys[pygame.K_SPACE] and self.cooldown <= 0:
+            laser = Laser(self.rect.centerx,self.rect.top,5,5)
+            lasers.append(laser)
+            self.cooldown = self.max_cooldown
+        elif self.cooldown > 0 :
+            self.cooldown -= 1
+
+class Bug(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+
+class Laser(pygame.rect.Rect):
+    def __init__(self,x,y,w,h,color=(255,0,255),speed = 9):
+        super().__init__(x,y,w,h)
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.color = color
+        self.speed = speed
+    def draw(self):
+        laser = pygame.rect.Rect(self.x,self.y,self.w,self.h)
+        pygame.draw.rect(screen,self.color,laser)
+    def update(self):
+        self.y -= self.speed
+        
 pro_ships = pygame.sprite.Group()
-ship = Ship(100,100,40,40,"ship.png",1,10)
+ship = Ship(100,100,27,33,"ship.png",1,10)
 pro_ships.add(ship)
+lasers = []
 
 while running:
     clock.tick(FPS)
@@ -45,6 +86,11 @@ while running:
     mouse_pos = pygame.mouse.get_pos()
     screen.fill(screen_color)
     pro_ships.draw(screen)
+    for laser in lasers:
+        laser.draw()
+        laser.update()
     for ship in pro_ships:
         ship.move()
+        ship.shoot()
+    
     pygame.display.flip()
